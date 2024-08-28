b1fb447fc4d90bee663527e78803752d26b4b8a9
ALIF-15 <alifkff01@gmail.com>
2024-08-29 01:19:05 +0700

    Update run.py

--- run.py
+++ run.py
@@ -1,20 +1,76 @@
-#!/usr/bin/env python3
-I=KeyboardInterrupt
-H=Exception
-F=str
-D='bold royal_blue1'
-C=exit
-try:from rich.console import Console as A;import os as B,time,re,json,requests;from rich.panel import Panel as G
-except(H,I)as E:from urllib.parse import quote;__import__('os').system(f"xdg-open https://wa.me/6283198075343?text=PREMIUM%20%3A%20{quote(F(E))}");C()
-class J:
-	def __init__(A):0
-	def Command(E):E.Banner();A(width=71,style=D).print('[italic red]\nYou can contact the admin so you can try this script, because the Apikey website service is having problems. You have to ask the admin to activate this command manually.\n');A().input('[bold white][[bold green]WhatsApp[bold white]]');time.sleep(5.);B.system(f"xdg-open https://wa.me/6283198075343?text=Hallo%20Bang%3F%0AI%20want%20to%20buy%20the%20cinsta%20script%21");C()
-	def Clear(A):B.system('cls'if B.name=='nt'else'clear');return'Sukses Membersihkan Terminal'
-	def Banner(B):B.Clear();A(width=71,style=D).print('[bold red]●[bold yellow] ●[bold green] ●[bold white]\n________ .___                 __          \n\\_   ___ \\|   | ____   _______/  |______   \n/    \\  \\/|   |/    \\ /  ___/\\   __\\__  \\  \n\\     \\___|   |   |  \\___ \\  |  |  / __ \\_\n \\______  /___|___|  /____  > |__| (____  /\n        \\/         \\/     \\/            \\/ \n        \n\t   Github.com/zhukov-z\n\t   t.me/a_life07');return'Sukses Menampilkan Banner'
-if __name__=='__main__':
-	try:
-		K=re.search('columns=(\\d+),',F(B.get_terminal_size())).group(1)
-		if int(K)<71:A(width=71,style=D).print(G(f"[italic red]Harap Kecilkan Tampilan Termux Dengan Cara Mencubit Layar Dalam Aplikasi Termux, Kecilkan Panel Ini Sampai Terlihat Rapi!",title='[bold red]>[bold yellow]>[bold green]>[bold royal_blue1] (Kecilkan Tampilan Termux) [bold green]<[bold yellow]<[bold red]<'));C()
-		else:B.system('git pull');J().Command()
-	except I:C()
-	except H as E:A(width=71,style=D).print(G(f"[italic red]{F(E).title()}!",title='[bold red]>[bold yellow]>[bold green]>[bold royal_blue1] (Error) [bold green]<[bold yellow]<[bold red]<'));C()
+import requests,os,time,json
+
+P = '\x1b[1;97m' # PUTIH
+M = '\x1b[1;91m' # MERAH
+H = '\x1b[1;92m' # HIJAU
+K = '\x1b[1;93m' # KUNING
+B = '\x1b[1;94m' # BIRU
+U = '\x1b[1;95m' # UNGU
+C = '\x1b[1;96m' # BIRU MUDA
+N = '\x1b[0m'	# WARNA MATI
+J = '\x1b[38;5;208m' #JINGGA
+G = "\033[1;32m"
+R = "\033[1;91m"
+W = "\033[1;97m"
+B = "\033[1;94m"
+
+def main():
+  try:
+    print(f"""{N}
+    \t{B} https://alif15.biz.id{N}
+    \t   admin@alif15.biz.id
+    
+    1. login key ( {K}+ setup {N})
+    2. buy key
+    0. {M}Exit.{N}
+    """)
+    menu = input(" @cinsta\> ")
+    if menu == "1":
+      key = input("\n @key\> ")
+      params = {'key': key,'device': 'cinsta'}
+      headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36',}
+      try:
+        kunci = requests.get('https://license.serv00.net/cek.php',params=params,headers=headers,)
+        if "eror" in kunci.text:
+          if "Key has expired" in kunci.text:
+            exit(" your key has expired")
+          elif "Device limit exceeded" in kunci.text:
+            exit(" login limit ")
+          else:
+            exit("eror")
+        elif "user" in kunci.text:
+          if "Device limit exceeded" in kunci.text: 
+            exit(" login limit ")
+          print(" @cinsta\> welcome to cinsta member")
+          #print(f"\n    name : {kunci.text}\n")
+          lo = requests.get(f"https://buy.alif15.biz.id/aktivasi={key}")
+          lo = "ok"
+          if "ok" in lo:
+            print(" kode akan exp dalam 10 menit")
+            kode = lo.json()["ok"]
+            print(f" code : {kode}")
+            url = f'https://pastebin.com/raw/{kode}'
+            response = requests.get(url)
+            if response.status_code == 200:
+              with open('run.py', 'wb') as f:
+                f.write(response.content)
+                print(" done silahkan ketik ulang,python run.py")
+          else:
+            exit("system eror")
+  
+        else:
+          exit(" key invalid,please contact admin")
+      except requests.exceptions.ConnectionError:
+        exit(" check internet")
+      pass
+    elif menu == "2":
+      exit("  akses web untuk melihat list harga")
+    else:
+      pass
+  except requests.exceptions.ConnectionError:
+    exit()
+    
+#def menu(key):
+  
+if __name__ == "__main__":
+  main()
